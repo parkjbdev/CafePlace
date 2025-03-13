9b954c31aa9d097a4b9321626550ed771ec827a9
parkjbdev <dev@parkjb.com>
2025-03-13 16:50:02 +0900

    build: fix

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
-brew install cocoapods node
+
+set -e
+echo "Running ci_post_clone.sh"
+
+cd ../../
+
+# Install Node, CocoaPods, and yarn using Homebrew.
+brew install node cocoapods yarn
+
+# Install dependencies
+yarn
+
+# See note above about patching for GetEnv Issue
+yarn add patch-package
+npx patch-package
+
+# xcode cloud sets `CI` env var to 'TRUE':
+# This causes a crash: Error: GetEnv.NoBoolean: TRUE is not a boolean.
+# This is a workaround for that issue.
+CI="true" npx expo prebuild
+
+cd ./ios
 pod install
